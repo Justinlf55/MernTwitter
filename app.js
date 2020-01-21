bd11445ad4ca49f09cae02fa435195867a232958
your name hereJustinlf55 <Justinlfern@gmail.com>
2020-01-21 10:34:10 -0800

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+require('./config/passport')(passport);
 
-app.get("/", (req, res) => {
-    res.send("Hello a/A")
-});
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+
+// app.get("/", (req, res) => {
+//     res.send("Hello a/A");
+// });
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 const port = process.env.PORT || 5000;
 
